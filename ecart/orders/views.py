ee8687bdbc90ed46ef633ef9c8177855ef863822
Amal <haris243659@gmail.com>
2022-07-25 17:14:28 +0530

    form

--- ecart/orders/views.py
+++ ecart/orders/views.py
@@ -144,8 +144,10 @@ def place_order(request, total=0, quantity=0):
                 'grand_total':grand_total,
             }
             return render(request, 'orders/payments.html', context)
+        
     else:
         return redirect('checkout')
+    
         
 
 
